da2992d6487e044f2bfe2910ea2b982a145ee3b2
Ishika-Sheth <ishika.sheth117715@marwadiuniversity.ac.in>
2024-09-23 21:09:56 +0530

    Add files via upload

---
+++ 2707. Extra Characters in a String.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    
+    def minExtraChar(self, s , dictionary):
+        n = len(s)
+        dictionarySet = set(dictionary)
+        # dp[i] := the minimum extra letters if breaking up s[0..i) optimally
+        dp = [0] + [n] * n
+
+        for i in range(1, n + 1):
+            for j in range(i):
+                if s[j:i] in dictionarySet:
+                    dp[i] = min(dp[i], dp[j])
+                else:
+                    dp[i] = min(dp[i], dp[j] + i - j)
+
+        return dp[n]
+
+        
